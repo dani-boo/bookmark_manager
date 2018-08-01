d7820610341c63b5df29242990cc194672791faf
Marcus <marcus.kerr94@gmail.com>
2018-08-01 16:07:47 +0100

    Step 8

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,10 +1,21 @@
 require 'bookmark'
+
 describe Bookmark do
   describe '#all' do
-    it 'shows all bookmarks' do
+    it 'shows all bookmarks in an array' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
       expect(Bookmark.all).to include('http://makersacademy.com')
-      expect(Bookmark.all).to include('http://google.com')
       expect(Bookmark.all).to include('http://destroyallsoftware.com')
+      expect(Bookmark.all).to include('http://google.com')
+
+      expected_bookmarks = [
+        'http://makersacademy.com',
+        'http://google.com',
+        'http://destroyallsoftware.com'
+      ]
+
+      expect(Bookmark.all).to eq expected_bookmarks
     end
   end
 end
--- spec/features/show_bookmars_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'Viewing bookmarks' do
-  scenario 'A user can see bookmarks' do
-    visit '/bookmarks'
-    expect(page).to have_content 'http://makersacademy.com'
-    expect(page).to have_content 'http://google.com'
-    expect(page).to have_content 'http://destroyallsoftware.com'
-  end
-end
